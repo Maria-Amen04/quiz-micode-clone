ec182ca78c75f98dd630c7b383286fc44272b49d
Maria-Amen04 <amendehoumon@gmail.com>
2025-04-07 14:45:56 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/quiz-micode-clone/",
+  build: {
+    outDir: "build", // Assurez-vous que le dossier de sortie est bien "build"
+  },
 });
